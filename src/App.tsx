905291b7d1213a8760ab5f849feb0cf5488ba163
wan6sta <wan6sta@gmail.com>
2023-01-22 19:40:56 +0300

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,26 @@ import { Todolist } from './components/Todolist/Todolist'
 import { ErrorAlert } from './components/ErrorAlert/ErrorAlert'
 import { PageLoader } from './components/PageLoader/PageLoader'
 import { AddNewTodo } from './components/AddNewTodo/AddNewTodo'
+import { apiInstance } from './shared/config/axiosInstance/axiosInstance'
 
 export const App = () => {
   const dispatch = useAppDispatch()
   const todos = useAppSelector(selectTodos)
 
+  const authMe = async () => {
+    const res = await apiInstance.get('/auth/me')
+    if (res.data.resultCode === 0) return
+
+    await apiInstance.post('/auth/login', {
+      email: 'free@samuraijs.com',
+      password: 'free',
+      rememberMe: true
+    })
+  }
+
   useEffect(() => {
     dispatch(fetchAsyncTodos())
+    authMe()
   }, [])
 
   return (
